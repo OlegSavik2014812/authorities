1db37c14971728c72c9d9c68046fa368f23aedd5
asavik <oleg.savik1996@gmail.com>
2019-08-19 18:35:03 +0300

    fix db migration bug

--- src/main/resources/db/migration/V4__Add_initial_information.sql
+++ src/main/resources/db/migration/V4__Add_initial_information.sql
@@ -17,7 +17,7 @@ insert into `groups`(name)
 values ('OWNER');
 
 insert into users(login, password, group_id)
-VALUES ('Oleg', '$2a$10$19.TUjECs76KbXEhJE4awutW9EOnv8lbq9Ya3q7pkYKn7ppukSueq', 2);
+VALUES ('OlegSavik', '$2a$10$19.TUjECs76KbXEhJE4awutW9EOnv8lbq9Ya3q7pkYKn7ppukSueq', 2);
 
 insert into users(login, password, group_id)
-values ('Test', '$2a$10$DqD9WIKBk8ffMJO8Mz266OTRvHgA9yFYZOchdhPq2kU5nqUmQ9rE2', 1);
+values ('Test123', '$2a$10$19.TUjECs76KbXEhJE4awutW9EOnv8lbq9Ya3q7pkYKn7ppukSueq', 1);
